c9a171044384f1f09066f3226a3cc5bc1fb916d1
taxze6 <1929509811@qq.com>
2022-01-03 10:23:10 +0800

    2022.01.03

---
+++ src/pers/taxze/arithmetic/everyday/Jan03.java
@@ -0,0 +1,30 @@
+package pers.taxze.arithmetic.everyday;
+/**
+ * 力扣2022年一月三日每日一题
+ * 题目链接：https://leetcode-cn.com/problems/day-of-the-week/
+ * 来源：力扣（LeetCode）
+ */
+
+/**
+ * 从1971年开始，非闰年365天，闰年366天
+ * 每个月的天数为[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]，闰年的二月为29天
+ * 1971-1-1为周五
+ */
+public class Jan03 {
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        /* 输入年份之前的年份的天数贡献 */
+        int days = 365 * (year - 1971) + (year - 1969) / 4;
+        /* 输入年份中，输入月份之前的月份的天数贡献 */
+        for (int i = 0; i < month - 1; ++i) {
+            days += monthDays[i];
+        }
+        if ((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) && month >= 3) {
+            days += 1;
+        }
+        /* 输入月份中的天数贡献 */
+        days += day;
+        return week[(days + 3) % 7];
+    }
+}
